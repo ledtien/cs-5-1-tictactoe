b449c2f448b92dbeac900c3c1aba4a064c76477c
Tien <ldtien.1993@gmail.com>
2021-04-19 16:08:40 +0700

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { useState } from "react";
 import "./App.css";
 
-const Square = ({ index }) => {
-  const [value, setValue] = useState(null);
+const Square = (props) => {
   return (
-    <button className="square" onClick={() => setValue("X")}>
-      {value}
+    <button
+      className="square"
+      onClick={() => props.onSquareClicked(props.index)}
+    >
+      {props.hi}
     </button>
   );
 };
-const Board = () => {
-  const renderSquare = (i) => {
-    return <Square index={i} />;
+const Board = (props) => {
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const onSquareClicked = (i) => {
+    if (props.squares[i]) {
+      return;
+    }
+    const newSquares = props.squares;
+    newSquares[i] = xIsNext ? "X" : "O";
+    props.setSquares(newSquares);
+    setXIsNext(!xIsNext);
   };
-  const status = "Next Player: X";
+
+  const renderSquare = (i) => {
+    return (
+      <Square
+        index={i}
+        hi={props.squares[i]}
+        onSquareClicked={onSquareClicked}
+      />
+    );
+  };
+  const status = xIsNext ? "Next Player: X" : "Next Player: O";
+
   return (
     <div>
       <div className="status">{status}</div>
@@ -37,20 +58,39 @@ const Board = () => {
   );
 };
 const Game = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <div>
       <div>
-        <Board />
+        <Board squares={squares} setSquares={setSquares} />
       </div>
       <div></div>
     </div>
   );
 };
 
+// const calculateWinner = () => {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// };
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App container">
       <Game />
     </div>
   );

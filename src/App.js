8d995cd36a56ce74ba6a659e5c1b0d4a245259ff
Tien <ldtien.1993@gmail.com>
2021-04-19 13:36:13 +0700

    Submit tictactoe exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+import "./App.css";
+
+const Square = ({ index }) => {
+  const [value, setValue] = useState(null);
+  return (
+    <button className="square" onClick={() => setValue("X")}>
+      {value}
+    </button>
+  );
+};
+const Board = () => {
+  const renderSquare = (i) => {
+    return <Square index={i} />;
+  };
+  const status = "Next Player: X";
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+const Game = () => {
+  return (
+    <div>
+      <div>
+        <Board />
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      <Game />
     </div>
   );
 }
